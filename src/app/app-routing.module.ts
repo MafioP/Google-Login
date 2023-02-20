03df3c0b1f627ada242187e7c7b77b5f43536f33
MafioP <42208309+MafioP@users.noreply.github.com>
2023-02-20 19:18:16 +0100

    Working auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './auth/auth.guard';
 import { LoggedPageComponent } from './logged-page/logged-page.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,3 +1,4 @@
+import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { Injectable, Input } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -6,10 +7,20 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  isLoggedIn: boolean = false;
+  constructor(private socialAuthService: SocialAuthService) {
+    socialAuthService.authState.subscribe((user) => {{
+      this.isLoggedIn = (user != null);
+      console.log('User is logged in ' + this.isLoggedIn);
+    }});
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if (!this.isLoggedIn) {
+        window.alert('User is not logged in!')
+      }
+    return this.isLoggedIn;
   }
 
 }
